1e83ae12cdbd6be52430681b6440a8648d308cba
Itzkacper12 <71788109+Itzkacper12@users.noreply.github.com>
2022-03-28 22:25:14 +0100

    [Addition] Added all the colours for the themes which all corresponded with a Kanye album.

--- yedle/Controllers/HomeController.cs
+++ yedle/Controllers/HomeController.cs
@@ -1,31 +1,34 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using yedle.Models;
 
-namespace yedle.Controllers;
-
-public class HomeController : Controller
+namespace yedle.Controllers
 {
-    private readonly ILogger<HomeController> _logger;
-
-    public HomeController(ILogger<HomeController> logger)
+    public class HomeController : Controller
     {
-        _logger = logger;
-    }
+        private readonly ILogger<HomeController> _logger;
 
-    public IActionResult Index()
-    {
-        return View();
-    }
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
 
-    public IActionResult Privacy()
-    {
-        return View();
-    }
+        public IActionResult Index()
+        {
+            return View();
+        }
 
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel {RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier});
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel {RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier});
+        }
     }
 }
+
--- yedle/Models/ErrorViewModel.cs
+++ yedle/Models/ErrorViewModel.cs
@@ -1,8 +1,9 @@
-namespace yedle.Models;
-
-public class ErrorViewModel
+namespace yedle.Models
 {
-    public string? RequestId { get; set; }
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
 
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }    
 }
