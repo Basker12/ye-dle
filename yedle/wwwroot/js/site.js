a873c49a6abc0bc4de3f0c7a1819debb1e4b70c3
Itzkacper12 <71788109+Itzkacper12@users.noreply.github.com>
2022-04-01 21:30:39 +0100

    [Addition] Most of the stats page is finished

--- yedle/wwwroot/js/site.js
+++ yedle/wwwroot/js/site.js
@@ -1,11 +1,24 @@
-﻿let btn = document.querySelector("#help");
+﻿let helpBtn = document.querySelector("#help");
 let helpDiv = document.querySelector("#boxHelp");
 
-btn.addEventListener('click', () => {
+let statsBtn = document.querySelector("#stats");
+let statsDiv = document.querySelector("#boxStats");
+
+
+helpBtn.addEventListener('click', () => {
     if (helpDiv.style.display === 'none') {
         helpDiv.style.display = 'block';
     } 
     else {
         helpDiv.style.display = 'none';
+    }
+});
+
+statsBtn.addEventListener('click', () => {
+    if (statsDiv.style.display === 'none') {
+        statsDiv.style.display = 'block';
+    }
+    else {
+        statsDiv.style.display = 'none';
     }
 });
