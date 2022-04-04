7b49f47e02337d53f1785be5e323a0cedc242c00
Itzkacper12 <71788109+Itzkacper12@users.noreply.github.com>
2022-04-04 20:15:45 +0100

    [Addition] I very quickly made the "outlines" of the rest of ye-dle this is so that i can start working on the search functionality

---
+++ yedle/wwwroot/js/processMusic.js
@@ -0,0 +1,11 @@
+﻿import MP3Cutter from './mp3-cutter';
+
+function cutmusic () {
+    MP3Cutter.cut({
+        src: 'yedle/Kanyes-Music/Kanye West & Kid Cudi - Kids See Ghosts (2018) - (www.SongsLover.Live)/03. 4th Dimension - (www.SongsLover.Live).mp3',
+        target: '4th Dimension (5sec)',
+        start: '0',
+        end: '1'
+    });
+    console.log("Your song was cut i think?")
+}
