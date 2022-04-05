4eed0930ee4133ced26ccf0cd0a34f0b473d2bb1
Itzkacper12 <71788109+Itzkacper12@users.noreply.github.com>
2022-04-05 22:23:52 +0100

    [Addition] Created a Sqlite3 database which contains all of Kanyes Songs

---
+++ yedle/KanyeModel/KanyeSongs.cs
@@ -0,0 +1,9 @@
+﻿namespace yedle.KanyeModel;
+
+public class KanyeSongs
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Artist { get; set; }
+    public string Album { get; set; }
+}
---
+++ yedle/KanyeModel/dbConnection.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace yedle.KanyeModel;
+
+public class dbConnection : DbContext
+{
+    public DbSet<KanyeSongs> Songs { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+        => options.UseSqlite(@"Data Source = C:\ye-dlee2\yedle\Database\KanyeSongs.db");
+}
